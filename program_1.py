2dcf01218ed5c9d5a0c6cae48065b370a9e22e31
Park1423 <thirdringdesign@hotmail.com>
2025-10-16 12:54:14 -0500

    Did 3 of the programs. Will do the other one after lunch.

--- program_1.py
+++ program_1.py
@@ -1,4 +1,28 @@
-# Program #1: Rainfall
-# Design a program that lets the user enter the total rainfall for each of 12 months into a list.
-# The program should calculate and display the total rainfall for the year, 
-# the average monthly rainfall, # and the months with the highest and lowest amounts.
+##
+# Code by: Parker Jolly
+# On: 10/16/2025
+# Program name: Rainfall
+##
+
+# Create rainfall list
+rainfall = []
+
+# Ask the user for the monthy rainfall 12 times and save it to the rainfall list.
+for i in range(12):
+    # Use a try/except to tellt the user if they gave bad input.
+    try:
+        rainfall.append(float(input("Enter the amount of rainfall for month " + str(i + 1) + ": ")))
+        
+    except ValueError:
+        print("Thats not a number.")
+        print("Ending program...")
+        break
+
+# Get the sum of rainfall for the total rainfall.
+print("The total rainfall for that year was " + str( sum(rainfall)))
+# Get the sum of rainfall divided by the length for the average rainfall.
+print("The average rainfall for that year was " + str( sum(rainfall) / len(rainfall) ))
+# Get the max number in the list and first occurance of that number to tell the user the month with the most rainfall and which month it was.
+print("The month with the most rainfall was month " + str( rainfall.index(max(rainfall)) + 1) + " with " + str( max(rainfall) ))
+# Repeat the last step for the lowest number.
+print("The month with the least rainfall was month " + str( rainfall.index(min(rainfall)) + 1) + " with " + str( min(rainfall) ))
--- program_2.py
+++ program_2.py
@@ -1,8 +1,8 @@
-# Program #2: Larger than n
-# In a program, write a function (with NO output) that accepts two arguments: number n, and a list.
-# Assume that the list contains numbers.
-# The function shell has been written out on line 30, (def display_larger_than_n_list)
-# and should display all of the numbers in the list that are greater than then number n.
+##
+# Code by: Parker Jolly
+# On: 10/16/2025
+# Program name: Larger than n
+##
 
 def main():
     # Declare local variables
@@ -28,8 +28,17 @@ def main():
 # a list, and a number. The function displays all of the numbers
 # in the list that are greater than the number.
 def display_larger_than_n_list(n, n_list):
-    # Write your code to display all of the numbers in the list that are greater than then number n. below
-    print('In display_larger_than_n_list')
+    # Create a empty list
+    values_bigger = []
+
+    # Iterate through the list passed to us and if the current 
+    # number is bigger than n, add it to out accumulaing list.
+    for current_number in n_list:
+        if current_number > n:
+            values_bigger.append(current_number)
+
+    # Output results.
+    print("The numbers in the list " + str(n_list) + " that are bigger than " + str(n) + " are " + str(values_bigger) + ".")
         
 # Call the main function.
 if __name__ == '__main__':
